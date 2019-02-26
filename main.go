7506a2aa69fd538913e6aa907cc764fbe08a9687
devforfu <developer.z@outlook.com>
2019-02-26 13:09:49 +0500

    Custom wrapper on top of flags

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-    "./src/unsplash"
+    "./src/cli"
     "./src/imutil"
+    "./src/unsplash"
     "encoding/json"
-    "flag"
-    "github.com/mitchellh/go-homedir"
     "io/ioutil"
     "log"
     "os"
-    "path"
     "strconv"
 )
 
@@ -22,100 +20,144 @@ func main() {
         log.Fatal("usage: gosplash <command> [<args>]")
     }
 
-    params := make(Params)
+    params := cli.Parse(os.Args)
     switch cmd := os.Args[1]; cmd {
     case "download":
-        log.Println("Downloading random images...")
-        params = ParseDownload()
-        client := unsplash.Client{
-            AccessKey:params["accessKey"].(string),
-            SecretKey:params["secretKey"].(string)}
-        output := params["output"].(string)
-        err := client.DownloadRandomPhotos(output, params["count"].(int))
-        log.Printf("Images downloaded into folder: %s\n", output)
-        if err != nil { log.Fatal(err) }
+        DownloadImages(params)
     case "thumb":
-        log.Fatal("Converting images into thumbnails...")
-        params = ParseThumbnails()
-        maker := imutil.NewThumbnailMaker(imutil.PNG, 128, 128)
-        output := params["output"].(string)
-        thumbs, err := maker.ConvertFolder(output, params["p"].(string))
-        if err != nil { log.Fatal(err) }
-        log.Println("Created thumbnails:")
-        n := len(strconv.Itoa(len(thumbs)))
-        for i, t := range thumbs {
-            log.Printf("\t%0*d: %s", n, i, t)
-        }
+        MakeThumbnails(params)
     case "canvas":
-        log.Fatal("Making a single image from list of thumbnails...")
-        // params := ParseCanvas()
+        ConcatIntoSingle(params)
     }
 }
 
-type Params map[string]interface{}
-
-func ParseDownload() Params {
-    confPath := flag.String(
-        "-conf",
-        "unsplash.key.json",
-        "path to the file with Unsplash API keys")
-
-    numOfImages := flag.Int(
-        "-n",
-        5,
-        "number of images to download")
-
-    outputPath := flag.String(
-        "-out",
-        path.Join(mustHomeDir(), "Unsplash"),
-        "path to the output folder")
-
-    flag.Parse()
-    data, err := ioutil.ReadFile(*confPath)
+func DownloadImages(params cli.Params) {
+    log.Println("Downloading random images...")
+    jsonConfig := MustLoadJSON(params["conf"].String())
+    client := unsplash.Client{
+        AccessKey:jsonConfig["accessKey"],
+        SecretKey:jsonConfig["secretKey"]}
+    err := client.DownloadRandomPhotos(params["out"].String(), params["n"].Integer())
     if err != nil { log.Fatal(err) }
+    log.Printf("Images downlaoded into folder: %s\n", params["out"].String())
+}
 
-    params := make(Params)
-    err = json.Unmarshal(data, &params)
+func MakeThumbnails(params cli.Params) {
+    log.Println("Converting images into thumbnails...")
+    maker := imutil.NewThumbnailMaker(imutil.PNG, 128, 128)
+    thumbs, err := maker.ConvertFolder(params["dir"].String(), params["p"].String())
     if err != nil { log.Fatal(err) }
-    params["count"] = *numOfImages
-    params["output"] = *outputPath
-    return params
+    log.Println("Created thumbnails:")
+    n := len(strconv.Itoa(len(thumbs)))
+    for i, t := range thumbs { log.Printf("\t%0*d: %s", n, i, t) }
 }
 
-func ParseThumbnails() Params {
-    dirname := flag.String(
-        "-dir",
-        mustWorkDir(),
-        "path to the folder with images")
-
-    pattern := flag.String(
-        "-p",
-        imutil.ImageFormats,
-        "image extensions pattern")
-
-    flag.Parse()
-    params := make(Params)
-    params["dirname"] = *dirname
-    params["pattern"] = *pattern
-    return params
+func ConcatIntoSingle(params cli.Params) {
+    log.Fatal("Not implemented!")
 }
 
-func ParseCanvas() Params {
-    dirname := flag.String("-dir", mustWorkDir(), "path to folder with images")
-    flag.Parse()
-    params := make(Params)
-    params["dirname"] = *dirname
-    return params
-}
-
-func mustHomeDir() string {
-    home, err := homedir.Dir()
+func MustLoadJSON(filename string) map[string]string {
+    data, err := ioutil.ReadFile(filename)
     if err != nil { log.Fatal(err) }
-    return home
+    config := make(map[string]string)
+    err = json.Unmarshal(data, &config)
+    if err != nil { log.Fatal(err) }
+    return config
 }
 
-func mustWorkDir() string {
-    workdir, err := os.Getwd()
-    if err != nil { log.Fatal(err) }
-    return workdir
-}
+//    switch cmd := os.Args[1]; cmd {
+//    case "download":
+//        log.Println("Downloading random images...")
+//        params = ParseDownload()
+//        client := unsplash.Client{
+//            AccessKey:params["accessKey"].(string),
+//            SecretKey:params["secretKey"].(string)}
+//        output := params["output"].(string)
+//        err := client.DownloadRandomPhotos(output, params["count"].(int))
+//        log.Printf("Images downloaded into folder: %s\n", output)
+//        if err != nil { log.Fatal(err) }
+//    case "thumb":
+//        log.Fatal("Converting images into thumbnails...")
+//        params = ParseThumbnails()
+//        maker := imutil.NewThumbnailMaker(imutil.PNG, 128, 128)
+//        output := params["output"].(string)
+//        thumbs, err := maker.ConvertFolder(output, params["p"].(string))
+//        if err != nil { log.Fatal(err) }
+//        log.Println("Created thumbnails:")
+//        n := len(strconv.Itoa(len(thumbs)))
+//        for i, t := range thumbs {
+//            log.Printf("\t%0*d: %s", n, i, t)
+//        }
+//    case "canvas":
+//        log.Fatal("Making a single image from list of thumbnails...")
+//        // params := ParseCanvas()
+//    }
+//}
+
+//type Params map[string]interface{}
+//
+//func ParseDownload() Params {
+//    confPath := flag.String(
+//        "-conf",
+//        "unsplash.key.json",
+//        "path to the file with Unsplash API keys")
+//
+//    numOfImages := flag.Int(
+//        "-n",
+//        5,
+//        "number of images to download")
+//
+//    outputPath := flag.String(
+//        "-out",
+//        path.Join(mustHomeDir(), "Unsplash"),
+//        "path to the output folder")
+//
+//    flag.Parse()
+//    data, err := ioutil.ReadFile(*confPath)
+//    if err != nil { log.Fatal(err) }
+//
+//    params := make(Params)
+//    err = json.Unmarshal(data, &params)
+//    if err != nil { log.Fatal(err) }
+//    params["count"] = *numOfImages
+//    params["output"] = *outputPath
+//    return params
+//}
+//
+//func ParseThumbnails() Params {
+//    dirname := flag.String(
+//        "-dir",
+//        mustWorkDir(),
+//        "path to the folder with images")
+//
+//    pattern := flag.String(
+//        "-p",
+//        imutil.ImageFormats,
+//        "image extensions pattern")
+//
+//    flag.Parse()
+//    params := make(Params)
+//    params["dirname"] = *dirname
+//    params["pattern"] = *pattern
+//    return params
+//}
+//
+//func ParseCanvas() Params {
+//    dirname := flag.String("-dir", mustWorkDir(), "path to folder with images")
+//    flag.Parse()
+//    params := make(Params)
+//    params["dirname"] = *dirname
+//    return params
+//}
+//
+//func mustHomeDir() string {
+//    home, err := homedir.Dir()
+//    if err != nil { log.Fatal(err) }
+//    return home
+//}
+//
+//func mustWorkDir() string {
+//    workdir, err := os.Getwd()
+//    if err != nil { log.Fatal(err) }
+//    return workdir
+//}
---
+++ src/cli/cli.go
@@ -0,0 +1,132 @@
+package cli
+
+import (
+    "../imutil"
+    "flag"
+    "fmt"
+    "github.com/mitchellh/go-homedir"
+    "log"
+    "os"
+    "path"
+    "strconv"
+)
+
+type ParameterType uint32
+
+const (
+    String ParameterType = iota
+    Integer
+    Boolean
+    Float
+)
+
+type Command struct {
+    Parameters []Parameter
+}
+
+func (c *Command) Parse() Params {
+    params := make(Params)
+    for _, p := range c.Parameters {
+        switch p.Type {
+        case String:
+            p.Value = flag.String(p.Name, p.Default.(string), p.Usage)
+        case Integer:
+            p.Value = flag.Int(p.Name, p.Default.(int), p.Usage)
+        case Boolean:
+            p.Value = flag.Bool(p.Name, p.Default.(bool), p.Usage)
+        case Float:
+            p.Value = flag.Float64(p.Name, p.Default.(float64), p.Usage)
+        default:
+            panic(fmt.Sprintf("Unknown parameter type: %v", p.Type))
+        }
+        params[p.Name] = p
+    }
+    flag.Parse()
+    for _, p := range params {
+        if p.Value == nil {
+            p.Value = p.Default
+        }
+    }
+    return params
+}
+
+type Parameter struct {
+    Name, Usage string
+    Type ParameterType
+    Value interface{}
+    Default interface{}
+}
+
+func (p Parameter) TypeName() string {
+    switch p.Type {
+    case String: return "String"
+    case Integer: return "Integer"
+    case Boolean: return "Boolean"
+    case Float: return "Float"
+    }
+    return "Unknown"
+}
+
+func (p Parameter) String() string {
+    if p.Type == String  { return p.Value.(string) }
+    if p.Type == Integer { return strconv.Itoa(p.Value.(int)) }
+    panic(fmt.Sprintf("cannot convert into string a parameter of type %s", p.TypeName()))
+}
+
+func (p Parameter) Integer() int {
+    if p.Type == Integer { return p.Value.(int) }
+    panic(fmt.Sprintf("cannot convert into integer a parameter of type %s", p.TypeName()))
+}
+
+func (p Parameter) Boolean() bool {
+    if p.Type == Boolean { return p.Value.(bool) }
+    panic(fmt.Sprintf("cannot convert into boolean a parameter of type %s", p.TypeName()))
+}
+
+func (p Parameter) Float() float64 {
+    if p.Type == Integer { return float64(p.Value.(int)) }
+    if p.Type == Float { return p.Value.(float64) }
+    panic(fmt.Sprintf("cannot convert into float a parameter of type %s", p.TypeName()))
+}
+
+type Params map[string]Parameter
+
+// -----------------------
+// Sub-commands definition
+// -----------------------
+
+func Parse(args []string) Params {
+    commands := map[string]*Command {
+        "download": {
+            Parameters: []Parameter{
+                {Name: "-conf", Type: String, Default: "unsplash.key.json", Usage: "path to the file with Unsplash API keys"},
+                {Name: "-n", Type: Integer, Default: 5, Usage: "number of images to download"},
+                {Name: "-out", Type: String, Default: path.Join(mustHomeDir(), "Unsplash"), Usage: "path to the output folder"},
+            },
+        },
+        "thumb": {
+            Parameters: []Parameter{
+                {Name:"-dir", Type:String, Default:mustWorkDir(), Usage:"path to a folder with images"},
+                {Name:"-p", Type:String, Default:imutil.ImageFormats, Usage:"image extensions pattern"},
+            },
+        },
+        "canvas": {
+            Parameters: []Parameter{
+                {Name:"-dir", Type:String, Default:mustWorkDir(), Usage:"path to a folder with images"},
+            },
+        },
+    }
+    return commands[args[1]].Parse()
+}
+
+func mustHomeDir() string {
+    home, err := homedir.Dir()
+    if err != nil { log.Fatal(err) }
+    return home
+}
+
+func mustWorkDir() string {
+    workdir, err := os.Getwd()
+    if err != nil { log.Fatal(err) }
+    return workdir
+}
